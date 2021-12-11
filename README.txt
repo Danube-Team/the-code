399ce6cf6f80c8044128211a1a2bb6dcbee1569e
MincyT <mincy.thankachan.2022@mumail.ie>
2021-12-11 19:31:38 +0000

    changes to wedding js
    added readme.txt, some changes to wedding js file

---
+++ README.txt
@@ -0,0 +1,13 @@
+About
+This App is to provide an mobile UI for app users to look for custom events and costs. It includes birthdays, meetings, baby showers and wedding events
+
+
+Developers/Contributors
+Team Danube - Maynooth University, Maynooth Ireland.
+
+
+Dependencies
+Datepicker - Customised data picker with CSS
+Validator - to validate email input
+
+
--- Wedding/AddWedding.js
+++ Wedding/AddWedding.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-//import { venueList } fom "./venueList.js";
 import validator from "validator";
 import weddingPhoto from "./wedding.png";
-
-//const localVenueList = venueList;
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "./styles.css";
 
 function isNumeric(num) {
   return !isNaN(num);
@@ -13,12 +13,10 @@ class AddWedding extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: 0,
+      // initialize all variables for states
       eventDate: new Date(),
       numOfGuests: 0,
       localVenueList: [],
-      isFetched: false,
-      errorMsg: null,
       venue: "",
       contact: "",
       phone: "",
@@ -28,8 +26,12 @@ class AddWedding extends Component {
       isBandChecked: false,
       isFlowersChecked: false,
       pricePP: 0,
+      
+      // used for fetching json file on github
+      isFetched: false,
+      errorMsg: null,
 
-      //error checking
+      // object store error messages 
       formErrors: {
         eventDate: "",
         numOfGuests: "",
@@ -38,9 +40,18 @@ class AddWedding extends Component {
         phone: "",
         email: ""
       },
-
+      // flags validating each input 
+      isContactValid: false,
+      isEventDateValid: false,
+      isVenueValid: false,
+      isPhoneValid: false,
+      isNumOfGuestsValid: false,
+      isEmailValid: false,
+      isformValid: false,
+      //array to store user inputs
       weddingArray: [
         {
+          id: 1,
           eventDate: "",
           numOfGuests: "",
           venue: "",
@@ -49,15 +60,10 @@ class AddWedding extends Component {
           email: "",
           totalPrice: 0
         }
-      ],
-      isContactValid: false,
-      isEventDateValid: false,
-      isVenueValid: false,
-      isPhoneValid: false,
-      isNumOfGuestsValid: false,
-      isEmailValid: false,
-      isformValid: false
+      ]
     };
+
+    // bind for event handlers
     this.submitButtonClick = this.submitButtonClick.bind(this);
     this.handleEventDate = this.handleEventDate.bind(this);
     this.handlenumOfGuests = this.handlenumOfGuests.bind(this);
@@ -72,26 +78,32 @@ class AddWedding extends Component {
     this.handleBandCheckbox = this.handleBandCheckbox.bind(this);
   }
 
+
+  // store input to an array - refresh array every time submit is clicked
   submitButtonClick(event) {
     let newArray = [
-      {eventdate : this.state.eventDate,
-      numOfGuests : this.state.numOfGuests,
-      venue : this.state.venue,
-      concat : this.state.contact,
-      phone : this.state.phone,
-      email : this.state.email,
-      totalPrice :
-      this.state.pricePP * this.state.numOfGuests +
-      (this.state.isDJChecked ? 300 : 0) +
-      (this.state.isFlowersChecked ? 200 : 0) +
-      (this.state.isBandChecked ? 200 : 0)
-
-
-    }];
-    this.state.weddingArray.push(newArray);
-    console.log(this.state.weddingArray);
+      { id: 1,
+        eventDate: this.state.eventDate,
+        numOfGuests: this.state.numOfGuests,
+        venue: this.state.venue,
+        contact: this.state.contact,
+        phone: this.state.phone,
+        email: this.state.email,
+        //calculate and store total price for selections
+        totalPrice:
+          this.state.pricePP * this.state.numOfGuests +
+          (this.state.isDJChecked ? 300 : 0) +
+          (this.state.isFlowersChecked ? 200 : 0) +
+          (this.state.isBandChecked ? 200 : 0)
+      }
+    ];
+    
+    this.state.weddingArray.splice(0, 1); // remove existing data in the array
+    this.state.weddingArray.push(newArray); // push new details to the array
+    console.log(this.state.weddingArray); // print contents of the array to console
   }
 
+  // fetch venue details from github where json file is stored
   async componentDidMount() {
     try {
       const API_URL =
@@ -106,23 +118,24 @@ class AddWedding extends Component {
     }
   }
 
-  handleEventDate(event) {
-    this.setState({ eventDate: event.target.value });
-    this.validateEventDate(event.target.value);
+  // store and validate user input
+  handleEventDate(eventdt) {
+    this.setState({ eventDate: eventdt });
+    this.validateEventDate(eventdt);
   }
 
   validateEventDate(eventDate) {
     let localFormErrors = this.state.formErrors;
 
-    if (eventDate < new Date()) {
-      localFormErrors.eventDate = "Event date cannot be less than today";
+    if (!(eventDate > new Date())) {
+      localFormErrors.eventDate = "Event date should be greater than today";
       this.setState({ isEventDateValid: false });
     } else {
       localFormErrors.eventDate = "";
       this.setState({ isEventDateValid: true });
     }
     this.setState({ formErrors: localFormErrors });
-    this.validateForm();
+    this.validateForm();  // validate form
   }
 
   handlenumOfGuests(event) {
@@ -144,23 +157,24 @@ class AddWedding extends Component {
   }
 
   handleVenueChange(e) {
-    this.setState({ venue: e.target.value });
+    // store selected venue name 
+    this.setState({ venue: this.state.localVenueList[e.target.value - 1].venuename });
     this.setState({ isVenueValid: true });
 
     this.setState({
-      pricePP: this.state.localVenueList[e.target.value-1].pricepp
+      pricePP: this.state.localVenueList[e.target.value - 1].pricepp
     });
     this.validateForm();
   }
 
   handleContactChange(event) {
     this.setState({ contact: event.target.value });
-    this.validateConcat(event.target.value);
+    this.validateContact(event.target.value);
   }
 
-  validateConcat(contact) {
+  validateContact(contact) {
     let localFormErrors = this.state.formErrors;
-    if (contact.length < 4) {
+    if (contact.length < 4) {  // check if minimum 4 characters are input by user
       localFormErrors.contact = "Contact name is too short or is invalid";
       this.setState({ isContactValid: false });
     } else {
@@ -179,7 +193,8 @@ class AddWedding extends Component {
   validatePhone(phone) {
     let localFormErrors = this.state.formErrors;
     console.log(isNumeric(phone));
-    if (phone.length < 8 || !isNumeric(phone)) {
+    // check for length and numeric only values for user input
+    if (phone.length < 8 || !isNumeric(phone)) {  
       localFormErrors.phone = "Phone is too short or is invalid";
       this.setState({ isPhoneValid: false });
     } else {
@@ -197,7 +212,7 @@ class AddWedding extends Component {
 
   validateEmail(email) {
     let localFormErrors = this.state.formErrors;
-
+    // used external dependency for validating email input
     if (!validator.isEmail(email)) {
       localFormErrors.email = "Email is invalid";
       this.setState({ isEmailValid: false });
@@ -225,6 +240,7 @@ class AddWedding extends Component {
     this.setState({ comments: value });
   }
 
+  // validate entire form
   validateForm() {
     this.setState({
       isformValid:
@@ -232,7 +248,6 @@ class AddWedding extends Component {
         this.state.isEventDateValid &&
         this.state.isNumOfGuestsValid &&
         this.state.isPhoneValid &&
-        this.state.isVenueValid &&
         this.state.isEmailValid
     });
   }
@@ -240,123 +255,152 @@ class AddWedding extends Component {
   render() {
     return (
       <div>
-        <p>
+        <header>
+          <div class="header">Wedding Event</div>
+        </header>
+        <hr />
+        <div class="frame1">
           <img src={weddingPhoto} alt="logo" />
-        </p>
-
-        <h2> Create a Wedding Event</h2>
-        <form>
+        </div>
+        <div class="frame6">
+          <h4> Create a Wedding Event</h4>
           <div className="input_container">
-            <label>
-              Event Date &nbsp;
-              <input
-                type="date"
-                value={this.state.eventDate}
-                onChange={(e) => this.handleEventDate(e)}
+            <form>
+            <label>Event Date</label>
+              <DatePicker
+                dateFormat="MMMM d, yyyy"
+                closeOnScroll={true}
+                selected={this.state.eventDate}
+                onChange={this.handleEventDate}
               />
-            </label>
+            </form>
             <br />
+            <form>
+              <label>
+                Guests &nbsp;
+                <input
+                  type="number"
+                  placeholder="Enter number of guests"
+                  value={this.state.numOfGuests}
+                  onChange={(e) => this.handlenumOfGuests(e)}
+                />
+              </label>
+            </form>
             <br />
-            <label>
-              Guests &nbsp;
-              <input
-                type="number"
-                placeholder="Enter number of guests"
-                value={this.state.numOfGuests}
-                onChange={(e) => this.handlenumOfGuests(e)}
-              />
-            </label>
+            <form>
+              <label>
+                Venue (Capacity) &nbsp;
+                <select onChange={(e) => this.handleVenueChange(e)}>
+                  {this.state.localVenueList.map((v, index) => (
+                    <option key={v.id} value={v.id}>
+                      {v.venuename} - ({v.capacity})
+                    </option>
+                  ))}
+                </select>
+              </label>
+            </form>
             <br />
-            <br />
-            <label>
-              Venue (Capacity) &nbsp;
-              <select onChange={(e) => this.handleVenueChange(e)}>
-                {this.state.localVenueList.map((v, index) => (
-                  <option key={v.id} value={v.id}>
-                    {v.venuename} - ({v.capacity})
-                  </option>
-                ))}
-              </select>
-            </label>
-            <br />
-            <br />
-            <label>
-              Contact Name &nbsp;
-              <input
-                type="text"
-                placeholder="Provide a Contact Name"
-                value={this.state.contact}
-                onChange={(e) => this.handleContactChange(e)}
-              />
-            </label>
+            <form>
+              <label>
+                Contact Name &nbsp;
+                <input
+                  type="text"
+                  placeholder="Provide a Contact Name"
+                  value={this.state.contact}
+                  onChange={(e) => this.handleContactChange(e)}
+                />
+              </label>
+            </form>
             &nbsp; &nbsp; &nbsp;
             <br />
+            <form>
+              <label>
+                Phone &nbsp;
+                <input
+                  type="text"
+                  placeholder="Contact Phone/Mobile"
+                  value={this.state.phone}
+                  onChange={(e) => this.handlePhoneChange(e)}
+                />
+              </label>
+            </form>
+            &nbsp; &nbsp; 
+            <form>
+              <label>
+                Email &nbsp;
+                <input
+                  type="text"
+                  placeholder="Email ID"
+                  value={this.state.email}
+                  onChange={(e) => this.handleEmailChange(e)}
+                />
+              </label>
+            </form>
             <br />
-            <label>
-              Phone &nbsp;
-              <input
-                type="text"
-                placeholder="Contact Phone/Mobile"
-                value={this.state.phone}
-                onChange={(e) => this.handlePhoneChange(e)}
-              />
-            </label>
-            &nbsp; &nbsp; &nbsp;
-            <label>
-              Email &nbsp;
-              <input
-                type="text"
-                placeholder="Email ID"
-                value={this.state.email}
-                onChange={(e) => this.handleEmailChange(e)}
-              />
-            </label>
-            <br />
-            <br />
-            Others &nbsp; &nbsp;
-            <input
-              type="checkbox"
-              onChange={(e) => this.handleFlowersCheckbox(e)}
-              defaultChecked={this.state.isFlowersChecked}
-            />
-            Flowers &nbsp;
-            <input
-              type="checkbox"
-              onChange={(e) => this.handleDJCheckbox(e)}
-              defaultChecked={this.state.isDJChecked}
-            />
-            DJ &nbsp;
-            <input
-              type="checkbox"
-              onChange={(e) => this.handleBandCheckbox(e)}
-              defaultChecked={this.state.isBandChecked}
-            />
-            Band
-            <label>
-              <br /> <br />
-              Special Requirements &nbsp;
-              <textarea
-                placeholder="Special notes if any"
-                value={this.state.comments}
-                onChange={(e) => this.handleCommentEvent(e)}
-              />
-            </label>
+            <form>
+              <div class="frame4">
+                <b>Others</b>
+                <label>
+                <input
+                  type="checkbox"
+                  onChange={(e) => this.handleFlowersCheckbox(e)}
+                  defaultChecked={this.state.isFlowersChecked}
+                />
+                Flowers
+                </label>
+                <label>
+                <input
+                  type="checkbox"
+                  value="DJ"
+                  onChange={(e) => this.handleDJCheckbox(e)}
+                  defaultChecked={this.state.isDJChecked}
+                />
+                DJ
+                </label>
+                <label>
+                <input
+                  type="checkbox"
+                  onChange={(e) => this.handleBandCheckbox(e)}
+                  defaultChecked={this.state.isBandChecked}
+                />
+                Band
+                </label>
+              </div>
+            </form>
             <br /> <br />
+            <form>
+              <label>
+                {" "}
+                Special Requirements &nbsp;
+                <textarea
+                  placeholder="Special notes if any"
+                  value={this.state.comments}
+                  onChange={(e) => this.handleCommentEvent(e)}
+                />
+              </label>
+            </form>
+            <br /> <br />
+            {/* Conditional rendering of total price 
+                once the venue is selected pricepp is set
+            */}
             {this.state.pricePP !== 0 && (
               <b>
-                {" "}
-                Total Price (€):{" "}
+                {" "}                
+                Total Price (€):{" "} 
                 {this.state.pricePP * this.state.numOfGuests +
                   (this.state.isDJChecked ? 300 : 0) +
                   (this.state.isFlowersChecked ? 200 : 0) +
                   (this.state.isBandChecked ? 200 : 0)}{" "}
               </b>
             )}
-            <br /> <br />
-            <span
+            { /* inline style for displaying error messages 
+                 for invalid input when validation fails
+            */}
+            <div
               style={{
                 fontWeight: "bold",
-                color: "red"
+                color: "red",
+                textAlign: "left"
               }}
             >
               {!this.state.isEventDateValid && this.state.formErrors.eventDate}
@@ -365,9 +409,10 @@ class AddWedding extends Component {
               {!this.state.isContactValid && this.state.formErrors.contact}
               {!this.state.isPhoneValid && this.state.formErrors.phone}
               {!this.state.isEmailValid && this.state.formErrors.email}
-            </span>
+            </div>
+            <br />
+            <br />
           </div>
-		 </form>
           <div className="frame4">
             <br />
             <button
@@ -375,9 +420,11 @@ class AddWedding extends Component {
               disabled={!this.state.isformValid}
               onClick={this.submitButtonClick}
             >
-              submit
+              Submit
             </button>
           </div>
+          <br />          
+        </div>
       </div>
     );
   }
